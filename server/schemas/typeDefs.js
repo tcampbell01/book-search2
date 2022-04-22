ca973bdc665d9501f909ecc709f54fa3e34f7a5d
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-04-22 12:54:05 -0400

    updated typeDefs.js saveBook

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -48,7 +48,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: SavedBook!): User
+    saveBook(input: savedBook!): User
    removeBook(bookId: ID!): User
   }
 `;
